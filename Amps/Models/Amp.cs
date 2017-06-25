535acbe4edd64a3d7d4c08b6dbe06148251673cf
Michael Pellegrini <mipelleg@microsoft.com>
2017-06-25 09:41:58 -0700

    Adds details about 3 fender amps

--- Amps/Models/Amp.cs
+++ Amps/Models/Amp.cs
@@ -11,6 +11,7 @@ namespace Amps.Models
         public string Make { get; set; }
         public string Model { get; set; }
         public string SpeakerConfiguration { get; set; }
+        public string SpeakerType { get; set; }
         public int Watts { get; set; }
         public int MSRP { get; set; }
         public string Url { get; set; }
@@ -18,5 +19,20 @@ namespace Amps.Models
         public decimal Width { get; set; }
         public decimal Depth { get; set; }
         public decimal Weight { get; set; }
+        public bool HasEffectsLoop { get; set; }
+        public bool HasReverb { get; set; }
+        public bool HasTremolo { get; set; }
+        public string PreampTubes { get; set; }
+        public string PowerampTubes { get; set; }
+        public string Rectifier { get; set; }
+        public Article[] Articles { get; set; }
+        public string[] Videos { get; set; }
+        public int Channels { get; set; }
+        public string Inputs { get; set; }
+        public string Outputs { get; set; }
+        public string Eq { get; set; }
+        public bool HasFootswitch { get; set; }
+        public string[] Pros { get; set; }
+        public string[] Cons { get; set; }
     }
 }
---
+++ Amps/Models/Article.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amps.Models
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
