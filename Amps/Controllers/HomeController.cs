34ff4f6888f8b3a3760e1b5c1d51a4790261fed9
Michael Pellegrini <mipelleg@microsoft.com>
2017-06-24 22:55:16 -0700

    Initial commit

--- Amps/Controllers/HomeController.cs
+++ Amps/Controllers/HomeController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amps.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Amps.Controllers
 {
@@ -10,26 +13,23 @@ namespace Amps.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
+            var viewModel = new List<Amp>();
+            var ampsCurationList = new[]
+            {
+                "fender-deville-ml",
+                "fender-68-custom-vibrolux",
+                "fender-68-custom-deluxe",
+                "fender-bassbreaker-18-30",
+                "fender-bassbreaker-45"
+            };
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
+            foreach (var slug in ampsCurationList)
+            {
+                var amp = JsonConvert.DeserializeObject<Amp>(
+                    System.IO.File.ReadAllText($@"C:\Users\mipelleg\Sites\Amps\Amps\Json\{slug}.json"));
+                viewModel.Add(amp);
+            }
+            return View("Views/Home/Index.cshtml", viewModel.ToArray());
         }
     }
 }
---
+++ Amps/Models/Amp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amps.Models
+{
+    public class Amp
+    {
+        public string Slug { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string SpeakerConfiguration { get; set; }
+        public int Watts { get; set; }
+        public int MSRP { get; set; }
+    }
+}
