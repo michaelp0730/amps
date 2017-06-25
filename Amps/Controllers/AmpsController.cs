f66876884d63c7fe4fb028a9b8f31a895b2f998a
Michael Pellegrini <mipelleg@microsoft.com>
2017-06-24 23:42:51 -0700

    Adds support for amp detail pages

---
+++ Amps/Controllers/AmpsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amps.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Amps.Controllers
+{
+    public class AmpsController : Controller
+    {
+        [Route("/amps/{slug}")]
+        public ViewResult AmpDetails(string slug)
+        {
+            var amp = JsonConvert.DeserializeObject<Amp>(
+                System.IO.File.ReadAllText($@"C:\Users\mipelleg\Sites\Amps\Amps\Json\{slug}.json"));
+            return View("Views/Amps/_details.cshtml", amp);
+        }
+    }
+}
--- Amps/Models/Amp.cs
+++ Amps/Models/Amp.cs
@@ -13,5 +13,10 @@ namespace Amps.Models
         public string SpeakerConfiguration { get; set; }
         public int Watts { get; set; }
         public int MSRP { get; set; }
+        public string Url { get; set; }
+        public decimal Height { get; set; }
+        public decimal Width { get; set; }
+        public decimal Depth { get; set; }
+        public decimal Weight { get; set; }
     }
 }
